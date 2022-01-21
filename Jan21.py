adb7314a2c71a2a44cf1ea9618cbcacc1be6e9f2
JackBoron12 <jack.boron@eaglebrook.org>
2022-01-21 11:11:53 -0500

    Jan21 classs work

---
+++ Jan21.py
@@ -0,0 +1,14 @@
+from turtle import *
+begin_fill()
+for i in range(10):
+
+
+    color("red","blue")
+
+    forward (100)
+    right (144)
+
+end_fill()
+
+
+done()
---
+++ jan11.py
@@ -0,0 +1,7 @@
+def jan11a( x ):
+    return 4 * x + 6
+
+jan11a()
+
+
+print (jan11a)
